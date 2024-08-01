740c6f61dd59cc84c7b2de30bab7419b926c1551
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-08-02 01:48:19 +0900

    DataStructure_Day2
    DataStructure_Day2 과제

---
+++ 자료구조/그래프/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Xml.Linq;
+
+namespace DataStructure_Day2
+{
+    #region 과제 1. 그래프 구현하기
+    internal class Program
+    {
+
+        public class Graph<T>
+        {
+            List<int> list;
+            List<List<int>> edge;
+            Dictionary<T,int> nameToId = new Dictionary<T,int>();
+            Dictionary<int, T> idToName = new Dictionary<int, T>();
+
+            public Graph()
+            {
+                list = new List<int>();
+                edge = new List<List<int>>();
+            }
+
+            public void AddNode(T name)
+            {
+                if (nameToId.ContainsKey(name) == false) 
+                {
+                    nameToId.Add(name, list.Count);
+                    idToName.Add(list.Count, name);
+                    list.Add(list.Count);
+                    edge.Add(new List<int>() );
+                }
+            }
+
+            public void AddEdge(T from , T to) 
+            {
+                if (nameToId.ContainsKey(from) && nameToId.ContainsKey(to))
+                {
+                    var fromId = nameToId[from];
+                    var toId = nameToId[to];
+                    edge[fromId].Add(toId);
+                }
+            }
+
+            public void PrintGraph()
+            {
+                for (int i = 0; i < edge.Count; i++)
+                {
+                    Console.WriteLine($"{idToName[i]}노드 :");
+                    foreach (var neighborId in edge[i])
+                    {
+                        Console.WriteLine($"    {idToName[neighborId]}노드");
+                    }
+                }                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Graph<int> graph = new Graph<int>();
+            for (int i = 0; i<8;i++)
+            {
+                graph.AddNode(i);
+            }
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 0);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 5);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(4, 7);
+            graph.AddEdge(5, 3);
+            graph.AddEdge(6, 7);
+            graph.AddEdge(7, 5);
+            graph.AddEdge(7, 6);
+
+            graph.PrintGraph();
+        }
+    }
+    #endregion
+}
---
+++ 자료구조/딕셔너리/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace DataStructure_Day2
+{
+    #region 과제 1. 몬스터 데이터베이스 구현하기
+    internal class Program
+    {
+        public class MonsterData
+        {
+            Dictionary<string, Monster> dic;
+            public MonsterData() 
+            { 
+                dic = new Dictionary<string, Monster>() {  };
+            }
+
+            public bool TryGet(string key, out Monster monster)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    monster = dic[key];
+                    return true;
+                }
+                else 
+                {
+                    monster = null;
+                    return false; 
+                }
+            }
+
+            public void AddData(float Hp,float Mp, string name)
+            {
+                Monster monster = new Monster(name);
+                monster.Hp = Hp;
+                monster.Mp = Mp;
+                dic[monster.Name] = monster;    
+            }
+        }
+
+        static public MonsterData data = new MonsterData();
+
+        public class Monster
+        {
+            public float Hp { get; set; }
+            public float Mp { get; set; }
+            public string Name { get; set; }
+            public Monster(string name)
+            {
+                if (data.TryGet(name, out Monster monster) == true)
+                {
+                    Hp = monster.Hp;
+                    Mp = monster.Mp;
+                }
+                Name = name;
+            }
+        }
+        static void Main(string[] args)
+        {
+            data.AddData(100f, 50f, "m1");
+            data.AddData(80f, 30f, "m2");
+            data.AddData(90f, 20f, "m3");
+            data.AddData(30f, 80f, "m4");
+            data.AddData(40f, 60f, "m5");
+
+            Monster monster = new Monster("m5");
+
+        }
+    }
+    #endregion
+}
